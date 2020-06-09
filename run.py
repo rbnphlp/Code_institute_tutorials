e920d109701044e7583942964fb698adc087849f
Robin Philip <rbnphlp@hotmail.com>
2020-06-09 20:58:11 +0000

    Routing using a base html

--- run.py
+++ run.py
@@ -20,6 +20,10 @@ def about():
     return(render_template("about.html"))
 
 
+@app.route("/careers")
+def careers():
+    return(render_template("careers.html"))
+
 if __name__=="__main__":
     app.run(host=os.environ.get("IP","0.0.0.0"),
     port=int(os.environ.get("PORT","5000")),debug=True)
